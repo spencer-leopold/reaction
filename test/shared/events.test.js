dfcd5f7857919a05262eab9e2ab29a2cb473e10e
Spencer Leopold <spencer.leopold@gmail.com>
2015-04-28 01:28:10 -0400

    added more event tests

--- test/shared/events.test.js
+++ test/shared/events.test.js
@@ -3,14 +3,38 @@ var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
 var Events = require('../../shared/events');
 var should = chai.should();
+var expect = chai.expect;
 
 chai.use(sinonChai);
 
 describe('shared/events', function() {
 
   describe('EventsDispatcher', function() {
+
     describe('#on()', function() {
+      beforeEach(function() {
+        fnSpy = sinon.spy();
+      });
+
+      afterEach(function() {
+        Events.remove('test');
+      });
+
       it('should create a new listener', function() {
+        Events.on('test', fnSpy);
+        expect(Events._listeners).to.have.a.property('test');
+        expect(Object.keys(Events._listeners).length).to.equal(1);
+      });
+
+      it('should use the passed in context', function() {
+        var ctx = {};
+        Events.on('test', fnSpy, ctx);
+        expect(Events._listeners).to.deep.equal({ test: [{ fn: fnSpy, ctx: ctx }] });
+      });
+
+      it('should fallback to current context', function() {
+        Events.on('test', fnSpy);
+        expect(Events._listeners).to.deep.equal({ test: [{ fn: fnSpy, ctx: Events }] });
       });
     });
 
@@ -21,16 +45,6 @@ describe('shared/events', function() {
 
     describe('#trigger()', function() {
       it('should call all listening events', function() {
-      });
-    });
-  });
-
-  describe('Dispatcher', function() {
-    describe('getInstance', function() {
-      it('should create one instance', function() {
-      });
-
-      it('should return the same instance', function() {
       });
     });
   });
