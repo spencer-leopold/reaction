dbba31e047d39047ef4181c7ba341f776a42c2f2
Spencer Leopold <spencer.leopold@gmail.com>
2015-06-10 01:41:48 -0400

    added restifyAdapter

--- server/restifyAdapter.js
+++ server/restifyAdapter.js
@@ -1,15 +1,10 @@
-var BaseServerAdapter = require('./base/serverAdapter');
+var BaseServerAdapter = require('reaction/server/base/serverAdapter');
 var util = require('util');
-var express = require('express');
 
-function RestifyAdapter(options, serverInstance) {
-  this.server = serverInstance;
-  this.expressRouter = express.Router();
+function RestifyAdapter(options, server) {
+  this.server = server;
 
-  BaseServerAdapter.call(this, options, serverInstance);
-
-  this.attachMiddleware();
-  this.attachRoutes();
+  BaseServerAdapter.call(this, options);
 
   return this.server;
 }
@@ -19,59 +14,25 @@ function RestifyAdapter(options, serverInstance) {
  */
 util.inherits(RestifyAdapter, BaseServerAdapter);
 
-RestifyAdapter.prototype.formatParams = function(path) {
-  return path;
+RestifyAdapter.prototype.attachRoutes = function(routes) {
+  var route, handler;
+
+  for (var i in routes) {
+    route = routes[i];
+    handler = this.buildHandler(route.options, 'send');
+    this.server.get(route.path, handler);
+  }
 }
 
-RestifyAdapter.prototype.addRoute = function(path, options) {
-  var handler;
-
-  if (options.handle) {
-    if (typeof options.handle !== 'function') {
-      throw new Error('Route handle must be a function');
-    }
-    else {
-      handler = function(request, reply) {
-        options.handle(request, reply);
-      }
-    }
-  }
-  else {
-    handler = function(request, reply) {
-      var attrs = request.attributes || {};
-      var templateVars = {
-        body: attrs.body,
-        appData: {
-          data: attrs.appData,
-          path: request.path
-        }
-      }
-
-      reply.render('index', templateVars);
-    }
-  }
-
-  this.expressRouter.get(path, handler);
+RestifyAdapter.prototype.attachServerFetcher = function() {
+  var middleware = this.loadServerFetcher();
+  this.server.use(middleware);
 }
 
-RestifyAdapter.prototype.attachMiddleware = function() {
-  var reactRoutes = this.router.routes;
-  var serverRoutePaths = this.serverRoutePaths;
-  var serverRoutesObj = this.serverRoutesObj;
-  var apiConfig = this.options.api;
-
-  var middlewareOptions = {
-    reactRoutes: reactRoutes,
-    serverRoutePaths: serverRoutePaths,
-    serverRoutesObj: serverRoutesObj
-  }
-
-  this.server.use(require('./middleware/expressFetch')(middlewareOptions));
-  this.server.use('/api', require('./middleware/apiProxy')(apiConfig));
-}
-
-RestifyAdapter.prototype.attachRoutes = function() {
-  this.server.use(this.expressRouter);
+RestifyAdapter.prototype.attachApiProxy = function() {
+  var middleware = this.loadApiProxy();
+  var apiPath = this.options.apiPath || '/api';
+  this.server.get(apiPath, middleware);
 }
 
 module.exports = RestifyAdapter;
